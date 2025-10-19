ba877a796507072ddbde4068ef991b4c5aa88540
Bluscream <Bluscream@users.noreply.github.com>
2025-10-19 06:28:24 +0200

    Update VRCX Plugin System - 2025-10-19 06:28:24

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ import { PluginRepo, PluginRepoManager, repoMetadata } from './modules/repo';
 
 // Initialize window.customjs
 window.customjs = {
-  build: "1760847209",
+  build: "1760847325",
   logColors: {
     CustomJS: "#00ff88",
     PluginLoader: "#2196f3",
